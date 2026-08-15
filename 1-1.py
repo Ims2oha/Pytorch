c69607b65dd1f3d4f475a0e9665119cb025c1e82
zizon-seoha <s26055@gsm.hs.kr>
2026-08-15 23:25:51 +0900

    텐서의 기본 연산

--- 1-1.py
+++ 1-1.py
@@ -11,5 +11,10 @@ print(A.shape)
 print(A.ndim)#차원의 수
 print(A.numel())#전체 원소의 수
 
-print(torch.zeros(5))#0으로 꽉찬 5차원 벡터 생성
-print(torch.zeros(5,2))
+print(torch.zeros(5))#0으로 꽉찬 ([5]) shape의 벡터 생성
+print(torch.zeros(5,2))#0으로 꽉찬 ([5,2]) shape의 행렬 생성
+print(torch.zeros_like(A))#A와 같은 shape의 0으로 채워진 행렬 생성
+print(torch.ones(5))#1로 꽉찬 ([5]) shape의 벡터 생성
+print(torch.arange(3,10,2))#range와 비슷하게 작동 3부터 시작하여서 10 전까지 2의 간격으로 벡터 생성
+print(torch.arange(0,1,0.1))#소숫점도 가능 range에서는 안됨
+print(torch.linspace(0,1,10))#0에서부터 1포함하여서 10개 생성
